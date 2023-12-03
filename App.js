26f95f86c53f9f1d1941ef8aa6232666b7c3b3c4
Nelli <nelli.kemi@gmail.com>
2023-12-03 18:36:00 +0200

    first updates

--- App.js
+++ App.js
@@ -1,403 +1,29 @@
+import 'react-native-gesture-handler';
 import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet, Button, Image, Pressable, TouchableOpacity } from 'react-native';
-import { BarCodeScanner } from 'expo-barcode-scanner';
-import Constants from "expo-constants";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import ChainInfo from './components/ChainInfo';
-import Timeline from 'react-native-timeline-flatlist';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Messages } from './screens/Messages';
+import { Account } from './screens/Account';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faInbox, faQrcode, faUser } from '@fortawesome/free-solid-svg-icons';
+import { Home } from './screens/Home';
+import { FetchInfo } from './screens/FetchInfo';
+import { MessageChain } from './screens/MessageChain';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { AuthContextProvider, useUser } from './Contexts';
+
+const rootStack = createStackNavigator();
+
+const Tab = createBottomTabNavigator();
 
 const Stack = createStackNavigator();
 
-const { manifest } = Constants;
-
-const uri = `http://${manifest.debuggerHost.split(':').shift()}:3000`;
-
-export default function App() {
-
-  const [recieved, setRecieved] = useState({});
-
-  function Splash({ navigation }) {
-
-    useEffect(() => {
-      setTimeout(() => {
-          navigation.replace('Home');
-      }, 2000);
-    }, []);
-
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Home screen</Text>
-      </View>
-    );
-  }
-
-  function Home({ navigation }) {
-
-  const [hasPermission, setHasPermission] = useState(null);
-  const [scanned, setScanned] = useState(false);
-  const [text, setText] = useState('Not yet scanned');
-  const [fetching, setFetching] = useState(false);
-
-  const askForCameraPermission = () => {
-    (async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })()
-  }
-
-  // Request Camera Permission
-  useEffect(() => {
-    askForCameraPermission();
-  }, []);
-
-  // What happens when we scan the bar code
-  const handleBarCodeScanned = ({ type, data }) => {
-    setScanned(true);
-    setText(data)
-    console.log('Type: ' + type + '\nData: ' + data)
-  };
-
-  const fetchInfo = () => {
-    const name = text.split("::")[1];
-    const id = text.split("::")[0];
-    console.log("data.id", text.id, "name", name)
-    if(!text || fetching){
-      console.log("Nope");
-    }else{
-      try{
-        setFetching(true);
-        console.log("Data here", name, id);
-        //switch url to your own address created with localtunnel. "test_aau" is the index to be looked for in this case
-        fetch("https://qr-code.loca.lt/api/messages/" + id)
-        //fetch("https://qr-code-server-2.loca.lt/api/messages/test_aau")
-        //fetch("https://qr-code.loca.lt/api/messages/aau_test_2")
-        //fetch("https://cvrapi.dk/api?search=25313763&country=dk")
-        .then(response => {
-          console.log("done");
-          return response.json()
-        }).then(data => {console.log(data), setRecieved(data)})
-        .then(setFetching(false));
-      }catch (error){
-        console.log(error)
-      }
-    }
-  }
-
-  // Check permissions and return the screens
-  if (hasPermission === null) {
-    return (
-      <View style={styles.container}>
-        <Text>Requesting for camera permission</Text>
-      </View>)
-  }
-  if (hasPermission === false) {
-    return (
-      <View style={styles.container}>
-        <Text style={{ margin: 10 }}>No access to camera</Text>
-        <Button title={'Allow Camera'} onPress={() => askForCameraPermission()} />
-      </View>)
-  }
-
-    return (
-      <View style={styles.container}>
-      <View style={styles.barcodebox}>
-        <BarCodeScanner
-          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-          style={{ height: 400, width: 400 }} />
-      </View>
-      <Text style={styles.maintext}>{text}</Text>
-
-      {scanned && <Pressable
-            style={styles.button}
-            onPress={() => {
-              navigation.navigate('Info', {name: text.split("::")[1]});
-              fetchInfo();}}
-          >
-            <Text style={{color: 'white'}}>Get info</Text>
-            </Pressable>}
-
-            {scanned && <Button title={'Scan again?'} onPress={() => {setScanned(false); setText("Not yet scanned");}} color='black' />}
-    </View>
-    )
-  }
-
-  function FarmInfo({route, navigation}){
-    const {actor} = route.params;
-    console.log(actor)
-    return (
-      <View style={{ flex: 1, marginHorizontal: 10}}>
-        <Text style={styles.sectionTitle}>Company name</Text>
-        <Text>{actor.name}</Text>
-        <Text style={styles.sectionTitle}>Company address</Text>
-        <Text>{actor.address}, {actor.city}</Text>
-        <Text style={styles.sectionTitle}>Feed</Text>
-        <Text>{actor.feed}</Text>
-        <Text style={styles.sectionTitle}>Feed from </Text>
-        <Text>{actor.feedorigin}</Text>
-        {actor.cert && <View style={{marginTop: 20 }} >
-          <Text style={styles.sectionTitle}>Certificate</Text>
-          <Pressable onPress={() => navigation.navigate('Certificate Info', {cert: actor.cert})}>
-            <Image style={{width: 40, height: 40}}
-              source={{
-                uri: 'https://qr-code-server-2.loca.lt/certified.jpg',
-              }}
-            />  
-          </Pressable>
-        </View>
-        }
-        <Button title="Go back" onPress={() => navigation.goBack()} />
-      </View>
-    );
-  }
-
-  function TransportInfo({route, navigation}){
-    const {actor} = route.params;
-    console.log(actor)
-    return (
-      <View style={{ flex: 1, marginHorizontal: 10}}>
-        <Text style={styles.sectionTitle}>Company name</Text>
-        <Text>{actor.name}</Text>
-        <Text style={styles.sectionTitle}>Company address</Text>
-        <Text>{actor.address}, {actor.city}</Text>
-        <Text style={styles.sectionTitle}>Transportation method</Text>
-        <Text>{actor.method}</Text>
-        <Text style={styles.sectionTitle}>Distance {actor.feedorigin}</Text>
-        <Text>{actor.distance}</Text>
-        {actor.cert && <View style={{marginTop: 20 }} >
-          <Text style={styles.sectionTitle}>Certificate</Text>
-          <Pressable onPress={() => navigation.navigate('Certificate Info', {cert: actor.cert})}>
-            <Image style={{width: 40, height: 40}}
-              source={{
-                uri: 'https://qr-code-server-2.loca.lt/certified.jpg',
-              }}
-            />  
-          </Pressable>
-        </View>
-        }
-        <Button title="Go back" onPress={() => navigation.goBack()} />
-      </View>
-    );
-  }
-
-  function AbattoirInfo({route, navigation}){
-    const {actor} = route.params;
-    console.log(actor)
-    return (
-      <View style={{ flex: 1, marginHorizontal: 10}}>
-        <Text style={styles.sectionTitle}>Company name</Text>
-        <Text>{actor.name}</Text>
-        <Text style={styles.sectionTitle}>Company address</Text>
-        <Text>{actor.address}, {actor.city}</Text>
-        <Text style={styles.sectionTitle}>Slaughter method</Text>
-        <Text>{actor.method}</Text>
-        {actor.cert && <View style={{marginTop: 20 }} >
-          <Text style={styles.sectionTitle}>Certificate</Text>
-          <Pressable onPress={() => navigation.navigate('Certificate Info', {cert: actor.cert})}>
-            <Image style={{width: 40, height: 40}}
-              source={{
-                uri: 'https://qr-code-server-2.loca.lt/certified.jpg',
-              }}
-            />  
-          </Pressable>
-        </View>
-        }
-        <Button title="Go back" onPress={() => navigation.goBack()} />
-      </View>
-    );
-  }
-
-  function ProcessingInfo({route, navigation}){
-    const {actor} = route.params;
-    console.log(actor)
-    return (
-      <View style={{ flex: 1, marginHorizontal: 10}}>
-        <Text style={styles.sectionTitle}>Company name</Text>
-        <Text>{actor.name}</Text>
-        <Text style={styles.sectionTitle}>Company address</Text>
-        <Text>{actor.address}, {actor.city}</Text>
-        <Text style={styles.sectionTitle}>Packaging material</Text>
-        <Text>{actor.packaging}</Text>
-        {actor.cert && <View style={{marginTop: 20 }} >
-          <Text style={styles.sectionTitle}>Certificate</Text>
-          <Pressable onPress={() => navigation.navigate('Certificate Info', {cert: actor.cert})}>
-            <Image style={{width: 40, height: 40}}
-              source={{
-                uri: 'https://qr-code-server-2.loca.lt/certified.jpg',
-              }}
-            />  
-          </Pressable>
-        </View>
-        }
-        <Button title="Go back" onPress={() => navigation.goBack()} />
-      </View>
-    );
-  }
-
-  function RetailerInfo({route, navigation}){
-    const {actor} = route.params;
-    console.log(actor)
-    return (
-      <View style={{ flex: 1, marginHorizontal: 10}}>
-        <Text style={styles.sectionTitle}>Company name</Text>
-        <Text>{actor.name}</Text>
-        <Text style={styles.sectionTitle}>Company address</Text>
-        <Text>{actor.address}, {actor.city}</Text>
-        <Text style={styles.sectionTitle}>Packaging material</Text>
-        <Text>{actor.packaging}</Text>
-        {actor.cert && <View style={{marginTop: 20 }} >
-          <Text style={styles.sectionTitle}>Certificate</Text>
-          <Pressable onPress={() => navigation.navigate('Certificate Info', {cert: actor.cert})}>
-            <Image style={{width: 40, height: 40}}
-              source={{
-                uri: 'https://qr-code-server-2.loca.lt/certified.jpg',
-              }}
-            />  
-          </Pressable>
-        </View>
-        }
-        <Button title="Go back" onPress={() => navigation.goBack()} />
-      </View>
-    );
-  }
-
-
-  function CertificateInfo({route, navigation}){
-    const {cert} = route.params;
-    return (
-      <View style={{flex: 1, marginHorizontal: 10}}>
-        <Text style={styles.mainTitle}>Certificate Name</Text>
-        <Image
-        style={{width: 40, height: 40}}
-        source={{
-          uri: 'https://qr-code-server-2.loca.lt/certified.jpg',
-        }}
-      />
-        <Text style={styles.sectionTitle}>Category</Text>
-        <Text>{cert.product_category}</Text>
-        <Text style={styles.sectionTitle}>Valid until</Text>
-        <Text>{cert.Valid_until}</Text>
-        <Text style={styles.sectionTitle}>Date of inspection</Text>
-        <Text>{cert.Date_of_annual_inspection}</Text>
-        <Text style={styles.sectionTitle}>Date of issuing</Text>
-        <Text>{cert.Date_of_issuing}</Text>
-        <Text style={styles.sectionTitle}>Place of issuing</Text>
-        <Text>{cert.Place_of_issuing}</Text>
-      </View>
-    );
-  }
-
-  function Info({ navigation , route}) {
-
-    const [js, setJS] = useState([{}]);
-    const [loading, setLoading] = useState(true);
-    const {name} = route.params;
-
-    useEffect(() => {
-      console.log(recieved, recieved.length);
-      //const json = JSON.parse(recieved);
-      setJS(recieved);
-      console.log("JS", recieved.fetched, recieved.length);
-      //console.log(json[0].did);
-      if(recieved.fetched || recieved.length > 1){
-        console.log("Data received");
-        console.log(recieved.length);
-
-        console.log(recieved);
-        setLoading(false);
-      }
-    }, [recieved]);
-
-    const onClickFarm= (actor) => {
-      navigation.navigate('Farm Info', {actor: actor});
-    };
-    const onClickTransport= (actor) => {
-      navigation.navigate('Transport Info', {actor: actor});
-    };
-    const onClickAbattoir= (actor) => {
-      navigation.navigate('Abattoir Info', {actor: actor});
-    };
-
-    const onClickProcessing= (actor) => {
-      navigation.navigate('Processing Info', {actor: actor});
-    };
-
-    const onClickRetailer= (actor) => {
-      navigation.navigate('Retailer Info', {actor: actor});
-    };
-
-    const formatDate = (date) => {
-      console.log(date)
-      const dd = date.getDate();
-      const mm = date.getMonth();
-      const yyyy = date.getFullYear();
-      const result = `${dd}.${mm}.${yyyy}`;
-      return result;
-    }
-
-    const onEventPressed = (e) => {
-      if(e.index === 0){
-        onClickFarm(js[e.index]);
-      }else if(e.index === 2){
-        onClickAbattoir(js[e.index]);
-      }else if(e.index === 4){
-        onClickProcessing(js[e.index]);
-      }else if(e.index === 6){
-        onClickRetailer(js[e.index]);
-      }else if(e.index === 1 || e.index === 3 || e.index === 5){
-        onClickTransport(js[e.index]);
-      }else{
-        //should never happen, do nothing
-      }
-    }
-
-    const getData = () => {
-      return ([
-      {time: formatDate(new Date(js[0].recordTime)), title: js[0].name, description: "Farm", index: 0},
-      {time: formatDate(new Date(js[1].recordTime)), title: js[1].name, description: "Transportation", index: 1},
-      {time: formatDate(new Date(js[2].recordTime)), title: js[2].name, description: "Abattoir", index: 2},
-      {time: formatDate(new Date(js[3].recordTime)), title: js[3].name, description: "Transportation", index: 3},
-      {time: formatDate(new Date(js[4].recordTime)), title: js[4].name, description: "Processing", index: 4},
-      {time: formatDate(new Date(js[5].recordTime)), title: js[5].name, description: "Transportation", index: 5},
-      {time: formatDate(new Date(js[6].recordTime)), title: js[6].name, description: "Retailer", index: 6}
-    ])}
-
-
-    console.log("loading", loading, "fetched", recieved.fetched)
-    return (loading === true ? <>
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Fetching information...</Text>
-      </View>
-      </> : recieved.fetched ? <>
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>{recieved.error}</Text>
-        <Button title="Try again" onPress={() => {setRecieved({}); navigation.goBack();}} />
-      </View>
-      </> :
-      <View style={{ flex: 1, marginHorizontal: 10}}>
-        <Text style={styles.mainTitle}>{name}</Text>
-        <Text style={styles.sectionTitle}>Origin</Text>
-        <Text style={{marginBottom: 20}}>{js[0].city}</Text>
-        <Text style={styles.sectionTitle}>Total distance travelled</Text>
-        <Text style={{marginBottom: 10}}>{js[1].distance + js[3].distance + js[5].distance}km</Text>
-        <Text style={styles.sectionTitle}>Supply Chain</Text>
-        <Timeline data={getData()} style={{width: '100%'}} onEventPress={onEventPressed}/>
-        <Button title="Go back" onPress={() => {setRecieved({}); navigation.goBack();}} />
-      </View>
-    );
-  }
-
-  // Return the View
-  return (
-    <NavigationContainer>
-      <Stack.Navigator 
-        initialRouteName='Splash'
-      >
-        <Stack.Screen
-          name="Splash"
-          component={Splash}
-        />
-        <Stack.Screen
+function ScanFlow() {
+  return(
+    <Stack.Navigator>
+      <Stack.Screen
           name="Home"
           component={Home}
           options={{
@@ -407,38 +33,140 @@ export default function App() {
         />
         <Stack.Screen
           name="Info"
-          component={Info}
+          component={FetchInfo}
+          options={{
+            headerTintColor: 'black',
+            headerStyle: { backgroundColor: 'white' },
+          }}
+        />
+    </Stack.Navigator>
+  )
+}
+
+function MessageStack() {
+  return(
+    <Stack.Navigator>
+      <Stack.Screen
+          name="Messages"
+          component={Messages}
+          options={{
+            headerTintColor: 'black',
+            headerStyle: { backgroundColor: 'white' },
+          }}
         />
         <Stack.Screen
-          name="Farm Info"
-          component={FarmInfo}
+          name="MessageChain"
+          component={MessageChain}
+          options={{
+            headerTintColor: 'black',
+            headerStyle: { backgroundColor: 'white' },
+            title: 'Messages'
+          }}
         />
-        <Stack.Screen
-          name="Transport Info"
-          component={TransportInfo}
-        />
-        <Stack.Screen
-          name="Abattoir Info"
-          component={AbattoirInfo}
-        />
-        <Stack.Screen
-          name="Processing Info"
-          component={ProcessingInfo}
-        />
-        <Stack.Screen
-          name="Retailer Info"
-          component={RetailerInfo}
-        />
-        <Stack.Screen
-          name="Certificate Info"
-          component={CertificateInfo}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    </Stack.Navigator>
+  )
+}
+
+function BottomTabs() {
+  return (
+    <Tab.Navigator
+    screenOptions={
+      ({route}) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === 'ScanFlow') {
+            iconName = faQrcode;
+            size = focused ? 25: 20;
+          } else if (route.name === 'MessageStack') {
+            iconName = faInbox;
+            size = focused ? 25: 20;
+          } else {
+            iconName = faUser;
+            size = focused ? 25: 20;
+          }
+          return(
+            <FontAwesomeIcon icon={iconName} size={size} color={color} />
+          );
+        },
+        tabBarActiveTintColor: '#0080ff',
+        tabBarInactiveTintColor: '#777777',
+      })
+    }
+    >
+      <Tab.Screen
+        name={'ScanFlow'} 
+        component={ScanFlow} 
+        options = {{
+          headerShown: false,
+        }}
+      />
+      {useUser ?? <Tab.Screen 
+        name={'MessageStack'} 
+        component={MessageStack} 
+        options={{
+          headerShown: false,
+        }}
+      />}
+      <Tab.Screen 
+        name={'Account'} 
+        component={Account} 
+        options={{
+          headerTintColor: 'black',
+          headerStyle: { backgroundColor: 'white' },
+        }}
+      />
+    </Tab.Navigator>
+  )
+}
+
+export default function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  function Splash({ navigation }) {
+
+    useEffect(() => {
+      setTimeout(() => {
+          navigation.replace('BottomTabs', { screen: 'Home'});
+      }, 2000);
+    }, []);
+    
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text>Home screen</Text>
+        </View>
+    );
+  }
+
+  // Return the View
+  return (
+    <AuthContextProvider >
+      <SafeAreaView style={{ flex: 1}}>
+        <NavigationContainer>
+          <rootStack.Navigator 
+            initialRouteName='App Name'
+          >
+            <rootStack.Screen 
+              name="BottomTabs"
+              component={BottomTabs}
+              options = {{
+                headerShown: false,
+              }}
+            />
+            <rootStack.Screen
+              name="App Name"
+              component={Splash}
+              options = {{
+                headerShown: false,
+              }}
+            />
+          </rootStack.Navigator>
+        </NavigationContainer>
+      </SafeAreaView>
+    </AuthContextProvider>
   );
 }
 
-const styles = StyleSheet.create({
+export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
---
+++ Contexts.js
@@ -0,0 +1,48 @@
+import { createContext, useContext, useState } from "react";
+import jwt_decode from "jwt-decode";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export const AuthContext = createContext(); //context to keep values
+export const AuthContextDispatch = createContext(); //context for functions that modify values
+
+
+export function AuthContextProvider({ children }){
+
+    const [user, setUser] = useState(null);
+
+    const updateUser = async () => {
+        const token = await AsyncStorage.getItem("token");
+        if(token){
+            const userName = jwt_decode(token).name;
+            fetch("/users/api/" + userName)
+                .then(response => response.json())
+                .then(data => {
+                    setUser(data);
+                })
+        } else {
+            setUser(null);
+        }
+    }
+
+    //provide a way for other components to access methods and values
+    return (
+        <AuthContextDispatch.Provider value={updateUser}>
+            <AuthContext.Provider value={user}>
+                {children}
+            </AuthContext.Provider>
+        </AuthContextDispatch.Provider>
+    );
+};
+
+
+//provide user to all the components
+export function useUser() {
+    const user = useContext(AuthContext);
+    return user;
+};
+
+//provide update user function to all the components
+export function useUpdateUser() {
+    const updateUser = useContext(AuthContextDispatch);
+    return updateUser;
+}
